d6894dd5acc1c5a899596c9d35f6eab4f21bd42c
jiangy <jiangliu0316@outlook.com>
2025-01-15 23:28:33 +0800

    feat: init project

---
+++ src/main/java/com/jiangy/thinkinginmybatisplus/ThinkingInMybatisplusApplication.java
@@ -0,0 +1,13 @@
+package com.jiangy.thinkinginmybatisplus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThinkingInMybatisplusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThinkingInMybatisplusApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jiangy/thinkinginmybatisplus/ThinkingInMybatisplusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jiangy.thinkinginmybatisplus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThinkingInMybatisplusApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
